7aa89d6ecdef1aa7f30690bfaa01c7cd51259644
Joshua Henderson <joshua.henderson@microchip.com>
2019-06-19 20:47:00 -0700

    add Chipmunk2D chipdrop example
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ chipdrop/chipdrop.cpp
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "shape.h"
+#include "chipmunk/chipmunk.h"
+#include <cairo.h>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <egt/ui>
+#include <iostream>
+#include <map>
+#include <unistd.h>
+#include <vector>
+
+using namespace egt;
+using namespace std;
+
+static cpSpace* create_world(double width, double height)
+{
+    auto gravity = cpv(0, -100);
+    cpSpace* space = cpSpaceNew();
+    cpSpaceSetGravity(space, gravity);
+    cpSpaceSetSleepTimeThreshold(space, .5);
+
+    cpBody* b = cpSpaceGetStaticBody(space);
+
+    cpShape* ground = cpSegmentShapeNew(b,
+                                        cpv(-width, 0),
+                                        cpv(width, 0),
+                                        0);
+
+    cpShapeSetFriction(ground, 1);
+    cpSpaceAddShape(space, ground);
+    cpBodySetPosition(b, cpv(1, height));
+
+    cout << "created world of size " << width << "," << height << endl;
+
+    return space;
+}
+
+struct Box2DWindow : public TopWindow
+{
+    Box2DWindow()
+        : TopWindow()
+    {
+        auto label = make_shared<Label>("EGT Chipmunk 2D Physics",
+                                        alignmask::center);
+        auto font = label->font();
+        font.set_size(40);
+        label->set_font(font);
+        add(egt::center(label));
+
+        m_space = create_world(to_meter(size().width), to_meter(size().height)) ;
+    }
+
+    void handle(Event& event) override
+    {
+        TopWindow::handle(event);
+
+        switch (event.id())
+        {
+        case eventid::pointer_hold:
+        {
+            auto mouse = display_to_local(event.pointer().point);
+            auto shape = make_shared<Shape>(m_space, mouse);
+
+            cout << "created " << shape->box() << endl;
+            add(shape);
+            m_boxes.push_back(shape);
+
+            shape->toss();
+            break;
+        }
+        case eventid::pointer_click:
+        {
+            auto mouse = display_to_local(event.pointer().point);
+            auto shape = make_shared<Shape>(m_space, mouse);
+
+            cout << "created " << shape->box() << endl;
+            add(shape);
+            m_boxes.push_back(shape);
+            break;
+        }
+        case eventid::pointer_dblclick:
+            for (auto i = m_boxes.begin(); i != m_boxes.end(); ++i)
+                (*i)->detach();
+            m_boxes.clear();
+            damage();
+            break;
+        default:
+            break;
+        }
+    }
+
+    void update()
+    {
+        experimental::code_timer(true, "step: ", [&]()
+        {
+            cpFloat timeStep = 1.0 / 30.0;
+            cpSpaceStep(m_space, timeStep);
+        });
+
+        for (auto i = m_boxes.begin(); i != m_boxes.end();)
+        {
+            (*i)->update();
+
+            bool visible = Rect::intersect(box(), (*i)->box());
+            if (!visible)
+            {
+                (*i)->detach();
+                i = m_boxes.erase(i);
+            }
+            else
+                ++i;
+        }
+    }
+
+    virtual ~Box2DWindow() = default;
+
+protected:
+
+    cpSpace* m_space;
+    vector<shared_ptr<Shape>> m_boxes;
+};
+
+int main(int argc, const char** argv)
+{
+    Application app(argc, argv, "drop");
+
+    Box2DWindow win;
+
+    PeriodicTimer timer(std::chrono::milliseconds(30));
+    timer.on_timeout([&win]()
+    {
+        win.update();
+    });
+    timer.start();
+
+    win.show();
+
+    return app.run();
+}
---
+++ chipdrop/shape.h
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include "chipmunk/chipmunk.h"
+#include <cairo.h>
+#include <egt/ui>
+#include <iostream>
+#include <map>
+
+using namespace egt;
+using namespace std;
+
+inline double to_meter(double x)
+{
+  //return x;
+    return x * .1;
+}
+
+inline double from_meter(double x)
+{
+  //return x;
+    return x * 10.;
+}
+
+/**
+ * A special widget that supports rotating a rectangle within it.
+ */
+class RotateRectangleWidget : public Widget
+{
+public:
+
+    explicit RotateRectangleWidget(const Color& fill)
+    {
+	set_border(2);
+	set_theme(theme());
+	set_color(Palette::ColorId::bg, fill);
+    }
+
+    void set_angle(double angle)
+    {
+      if (detail::change_if_diff<>(m_angle, angle))
+	damage();
+    }
+
+    virtual void draw(Painter& painter, const Rect&) override
+    {
+        auto cr = painter.context().get();
+        cairo_translate(cr, center().x, center().y);
+        cairo_rotate(cr, m_angle);
+        cairo_translate(cr, -center().x, -center().y);
+
+	Rect drawbox(Point(), m_shapesize);
+	drawbox.move_to_center(center());
+
+	theme().draw_box(painter,
+			 Theme::boxtype::blank | Theme::boxtype::solid,
+			 drawbox,
+			 color(Palette::ColorId::border),
+			 color(Palette::ColorId::bg),
+			 border(),
+			 margin());
+    }
+
+protected:
+    double m_angle{0.0};
+    Size m_shapesize;
+};
+
+class Shape : public RotateRectangleWidget
+{
+public:
+    Shape(cpSpace *space, const Point& point);
+
+    std::unique_ptr<Widget> clone()
+    {
+        return std::unique_ptr<Widget>();
+    }
+
+    void update();
+
+    bool is_alive() const
+    {
+        return true;
+    }
+
+    void toss()
+    {
+	cpBodySetVelocity(m_body, cpv(50,10));
+	cpBodySetAngularVelocity(m_body, -4.);
+    }
+
+    virtual ~Shape();
+
+protected:
+
+    cpSpace* m_space;
+
+    cpBody* createBoxElement(const cpVect& size,
+                             const cpVect& position);
+
+private:
+
+    cpBody* m_body;
+};
+
+#endif
